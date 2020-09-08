197fefa7d9f7a5204fdb326e1b9d78e1625c3c91
YulianaVAL <kvalencia373@gmail.com>
2020-09-08 18:15:49 -0500

    nav, form

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body class="bg-dark">
+
+<header>
+<nav class="navbar navbar-expand-lg  navbar-light bg-light fixed-top">
+
+<img src="img/cow.png" width="30" height="30" alt="imgicono" loading="lazy">
+            <h1 class="navbar-menu"></h1>Supermercados la vaquita SA</h1>
+            <a class="navbar-brand" href="index.php">
+                
+              </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+              
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                      <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active">
+                      <a class="nav-link" href="index.html">link <span class="sr-only">(current)</span></a>
+                    </li>
+                  </ul>
+                  
+                  
+                </div>
+              </nav>
+            
+
+            
+          </nav>
+</header>
+
+<main>
+    <br>
+    <br>
+    <br>
+
+<div class="conteiner">
+    <div class="row justify-content-center ">
+        <div class="col-4 ">
+            <form action="">
+                <label for="producto1">producto1</label>
+                <input type="text" class="form-control" id="producto1">
+
+                <label for="producto2">producto2</label>
+                <input type="text" class="form-control" id="producto2">
+
+                <label for="producto3">producto3</label>
+                <input type="text" class="form-control" id="producto3">
+
+                <label for="producto4">producto4</label>
+                <input type="text" class="form-control" id="producto4">
+
+                <label for="producto5">producto5</label>
+                <input type="text" class="form-control" id="producto5">
+                <br>
+
+                <button type="submit" class="btn btn-primary btn-block">calcular</button>
+                
+            </form>
+
+        </div>
+
+    </div>
+
+</div>
+
+</main>
+
+<footer>
+
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</body>
+</html>
