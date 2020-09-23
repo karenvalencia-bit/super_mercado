a5ccc288556254bab14fc63489712f8927e76df6
karen <karenochoa196@gmail.com>
2020-09-22 20:29:27 -0500

    terminado1
    esta terminado, pero mejorar un poco

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 <link rel="stylesheet" href="stilos.css">
+<link href="https://fonts.googleapis.com/css2?family=Special+Elite&display=swap" rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
     <title>Supermercado</title>
@@ -14,8 +15,8 @@
 
 
             <a class="navbar-brand" href="index.php">
-            <img src="img/cow.png" width="30" height="30" alt="imgicono" loading="lazy">
-            Supermercados la vaquita SA
+            <img src="img/despensa.png" width="30" height="30" alt="imgicono" loading="lazy">
+            Supermercado RICA DESPENSA
             
               </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -25,10 +26,10 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                   <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
-                      <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                      <a class="nav-link" href="index.php">inicio <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item active">
-                      <a class="nav-link" href="">logica<span class="sr-only">(current)</span></a>
+                      <a class="nav-link" href="compra.php">compras<span class="sr-only">(current)</span></a>
                     </li>
                   </ul>
                   
@@ -38,20 +39,20 @@
             
 
             
-          </nav>
+          
 </header>
 
-<main class= "text-white">
+<main class= "text-white ">
 <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="img/super1.jpg" class="d-block w-100" alt="...">
+      <img src="img/super1.jpg" class="d-block w-100" alt="carusel1">
     </div>
     <div class="carousel-item">
-      <img src="img/super2.jpg" class="d-block w-100" alt="...">
+      <img src="img/super2.jpg" class="d-block w-100" alt="carusel2">
     </div>
     <div class="carousel-item">
-      <img src="img/super3.jpg" class="d-block w-100" alt="...">
+      <img src="img/super3.jpg" class="d-block w-100" alt="carusel3">
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
@@ -64,25 +65,25 @@
   </a>
 </div>
      
-        <div class="container bg-secondary" >
+        <div class="container  " >
         
-        <div class="row">
-          <div class="col strow">
+        <div class="row ">
+          <div class="col strow ">
             <h1>BIENVENIDO a la tienda virtual.....</h1>
-          <p >en esta pagina encontraras productos de la tienda</p>
+          <p >en esta pagina encontraras algunos de los productos de la tienda</p>
           </div>
         </div>
 
-        <div class="row fondo">
-        <div class="col-12 col-md-6  ">
+        <div class="row fondo border ">
+        <div class="col-12 col-md-6 ">
         <h2>todos los viernes del mes son locos...</h2>
         <img src="img/oferta.png" alt="img1" class="img-fluid">
         <br>
         <br>
         </div>
-        <div class="col-12 col-md-6 fondo2 ">
-          <p class="texto1 mt-3">aprovecha las grandes ofertas que se vienen</p>
-          <p>para regalar el dia de la amistad, hay anchetas desde 10.000 hasta 100.000,
+        <div class="col-12 col-md-6 fondo2 border text-dark ">
+          <p class="texto1 text-dark mt-3">aprovecha las grandes ofertas que se vienen</p>
+          <p>para regalar el mes de la amistad, hay anchetas desde 10.000 hasta 100.000,
             los regalos que pase de 100.000 tendran un descuento especial porque siempre 
             pensamos en el cliente. </p>
             <p> estan tambien las ceverzas : coronitas, pilsen, aguila, garrafas de aguardiente, por la 
@@ -97,127 +98,215 @@
           
 
         </div>
+        
         </div>
+
+        <div class="container mt-5 border fondop">
         <h1  class="titulo mt-4">CONOZCA NUESTROS PRODUCTOS</h1>
         <div class="row">
-          <div class="col-12 col-md-4">
+        <div class="card-deck margen">
+  <div class="card  col-12 col-md-4">
+    <img src="img/Leche.jpg" class="card-img-top img1 img-fluid" alt="img4">
+    <div class="card-body">
+      <h4 class="card-title">leche-colanta</h4>
+      <p class="card-text p2"> precio antes: $ 2.500</p>
+      <p class="card-text p2 "> precio despues: $ 2.000</p>
+      <p class="card-text p2"> ahorro: $ 500</p></p>
+    </div>
+  </div>
+  <div class="card  col-12 col-md-4">
+    <img src="img/arroz.png" class="card-img-top img1 img-fluid" alt="img5">
+    <div class="card-body">
+      <h4 class="card-title">arroz Diana</h4>
+      <p class="card-text p2"> precio antes: $ 2.000</p>
+      <p class="card-text p2 "> precio despues: $ 1.800</p>
+      <p class="card-text p2"> ahorro: $ 200</p>
+    </div>
+  </div>
+  <div class="card col-12 col-md-4">
+    <img src="img/pan.jpg" alt="img6" class="card-img-top img-fluid img1" >
+    <div class="card-body">
+      <h4 class="card-title">pan bimbo-integral</h4>
+      <p class="card-text p2"> precio antes: $ 4.500</p>
+      <p class="card-text p2 "> precio despues: $ 2.500</p>
+      <p class="card-text p2"> ahorro: $ 2.000</p>
+    </div>
+  
+  </div>
+</div>
+</div>
+<div class="row mt-3">
+        <div class="card-deck margen">
+  <div class="card  col-12 col-md-4">
+    <img src="img/aceite.png" alt="img7" class="card-img-top img1 img-fluid" >
+    <div class="card-body">
+      <h4 class="card-title">de la marca gourmet</h4>
+      <p class="card-text p2"> promocion x3 productos</p>
+      <p class="card-text p2 "> precio : $ 35.000</p>
+      <p class="card-text p2"> precio sin promocion: $ 65.000</p></p>
+    </div>
+  </div>
+  <div class="card col-12 col-md-4">
+    <img src="img/papel.jpg" alt="img8" class="card-img-top img1 img-fluid" >
+    <div class="card-body">
+      <h4 class="card-title">papel higienico</h4>
+      <p class="card-text p2"> precio antes: $ 20.000</p>
+      <p class="card-text p2 "> precio despues: $ 12.800</p>
+      <p class="card-text p2"> ahorro: $ 7.200</p>
+    </div>
+  </div>
+  <div class="card  col-12 col-md-4">
+    <img src="img/salsas.png"  alt="img9" class="card-img-top img-fluid img1" >
+    <div class="card-body">
+      <h4 class="card-title">salsas constancia</h4>
+      <p class="card-text p2"> promo x2: $ 8.000 </p>
+      <p class="card-text p2 "> precio cada una: $ 5.500</p>
+      <p class="card-text p2"> ahorro: $ 2.500</p>
+    </div>
+  
+  </div>
+</div>
+</div>
 
+<div class="row mt-3">
+        <div class=" card-deck margen">
+  <div class="card col-12 col-md-4 ">
+    <img src="img/legu.jpg" alt="img10" class="card-img-top img1 img-fluid" >
+    <div class="card-body">
+      <h4 class="card-title">legumbres</h4>
+      <p class="card-text p2"> promocion x3 productos</p>
+      <p class="card-text p2 "> precio : $ 35.000</p>
+      <p class="card-text p2"> precio sin promocion: $ 65.000</p></p>
+    </div>
+  </div>
+  <div class="card card col-12 col-md-4">
+    <img src="img/hor.jpg" alt="img11 class="card-img-top img1 img-fluid" >
+    <div class="card-body">
+      <h4 class="card-title">hortalizas</h4>
+      <p class="card-text p2"> precio antes: $ 20.000</p>
+      <p class="card-text p2 "> precio despues: $ 12.800</p>
+      <p class="card-text p2"> ahorro: $ 7.200</p>
+    </div>
+  </div>
+  <div class="card card col-12 col-md-4">
+    <img src="img/cereal.jpg" alt="img12" class="card-img-top img-fluid img1" >
+    <div class="card-body">
+      <h4 class="card-title">cereales</h4>
+      <p class="card-text p2"> promo x2: $ 8.000 </p>
+      <p class="card-text p2 "> precio cada una: $ 5.500</p>
+      <p class="card-text p2"> ahorro: $ 2.500</p>
+    </div>
+  
+  </div>
+</div>
+</div>
+<h2 class="mt-4">ELECTRODOMESTICOS</h2>
+<div class="row mt-3">
+        <div class=" card-deck margen">
+  <div class="card col-12 col-md-4 ">
+    <img src="img/fre.jpg" alt="img7" class="card-img-top img1 img-fluid" >
+    <div class="card-body">
+      <h4 class="card-title">freidora kalley</h4>
+      <p class="card-text p2"> promocion x3 productos</p>
+      <p class="card-text p2 "> precio : $ 35.000</p>
+      <p class="card-text p2"> precio sin promocion: $ 65.000</p></p>
+    </div>
+  </div>
+  <div class="card card col-12 col-md-4">
+    <img src="img/w.jpg" alt="img8" class="card-img-top img1 img-fluid" >
+    <div class="card-body">
+      <h4 class="card-title">waflera  Oster</h4>
+      <p class="card-text p2"> precio antes: $ 120.000</p>
+      <p class="card-text p2 "> precio despues: $ 100.800</p>
+      <p class="card-text p2"> ahorro: $ 20.000</p>
+    </div>
+  </div>
+  <div class="card card col-12 col-md-4">
+    <img src="img/electro.png"  alt="img9" class="card-img-top img-fluid img1" >
+    <div class="card-body">
+      <h4 class="card-title">combo Oster</h4>
+      <p class="card-text p2"> promo licuadora-plancha: $290.000 </p>
+      <p class="card-text p2 "> precio cada una: $ 250.000</p>
+      <p class="card-text p2"> ahorro: $ 40.000</p>
+    </div>
+  
+  </div>
+</div>
+</div>
+<br>
+<br>
+
+
+        </div>
+
+        
+        
+
+       
+          <div class="container fondop text-center-justify">
+          <h1 class="mt-5 titulo">CONOZCA MAS DE NOSOTROS COMO SUPER MERCADO</h1>
+          <p class="p1">Somos un negocio dinámico que se adapta a las nuevas formas de consumo, a partir de la innovación como eje de la evolución del retail, de la transformación digital para cuidar el tiempo del cliente, de nuevas experiencias y de la sostenibilidad, a partir de una estrategia que construye país.</p>
+          <p class="p1">- Innovación en formatos de valor y en nuevas formas de pago sin fricciones para el cuidado del cliente.</p>
+          <p class="p1">- Estrategia omnicliente, para ofrecer al cliente lo que quiere, cuando quiere, donde quiere.</p>
+          <p class="p1">- Monetización del tráfico, a través de los centros comerciales y programas de fidelidad. Además de los negocios complementarios.</p>
+          <p class="p1">- Sostenibilidad: transformación a partir de un negocio que genera valor compartido.</p>
+
+          <h3 class="mt-3 titulo">Misión y Valores</h3>
+          
+          <div class="row mt-3">
+            <div class="col-12 col-md-6">
+            
+          <p class="p1" >mision, trabajamos para que el cliente vuelva</p>
+          <img src="img/mision.jpg" alt="img13" class="img-fluid">
+
+            </div>
+            <div class="col-12 col-md-6 ">
+            <p class="p1">valores, innovacion, creatividad, empatia, honrades</p>
+
+            <img src="img/valo.jpg" alt="img14" class="img-fluid img1">
+
+            </div>
 
           </div>
 
+          <br><br>
         
-        
-       
-
-        </div>
-
-
-
-
-      <div class="conteiner">
-
-
-    <div class="row justify-content-center mt-5">
-        <div class="col-4 ">
-        <h3> Factura compra</h3>
-
-        <form action="index.php" method="POST">
-
-           <div class="row mt-2 ">
-             <div class="col">
-                 <input type="text" class="form-control" placeholder="producto 1" name="producto1">
-               </div>
-             <div class="col">
-                 <input type="text" class="form-control" placeholder="precio($)" name="precio1">
-             </div>
-           </div>
-           <div class="row mt-2">
-             <div class="col">
-                 <input type="text" class="form-control" placeholder="producto 2 " name="producto2">
-               </div>
-             <div class="col">
-                 <input type="text" class="form-control" placeholder="precio($)" name="precio2">
-             </div>
-           </div>
-           <div class="row mt-2">
-             <div class="col">
-                 <input type="text" class="form-control" placeholder="producto 3" name="producto3">
-               </div>
-             <div class="col">
-                 <input type="text" class="form-control" placeholder="precio($)">
-             </div>
-           </div>
-           <div class="row mt-2">
-             <div class="col">
-                 <input type="text" class="form-control" placeholder="producto 4" name="producto4">
-               </div>
-             <div class="col">
-                 <input type="text" class="form-control" placeholder="precio($)">
-             </div>
-           </div>
-           <div class="row mt-2">
-             <div class="col">
-                 <input type="text" class="form-control" placeholder="producto 5" name="producto2">
-               </div>
-             <div class="col">
-                 <input type="text" class="form-control" placeholder="precio($)">
-             </div>
-           </div>
-           <button type="submit" class="btn btn-primary btn-block mt-2" name=botonCalcular>calcular</button>
-         </form> 
-
-        <?php
-         if(isset($_POST["botonCalcular"])):
-        ?>
-
-      <div class="text-white bg-danger">
-
-          <h5 class="mt-5"> 
-          <?php
-                   
-                  $precio1=$_POST["precio1"];
-                  $producto1=$_POST["producto1"];
-
-                  $precio2=$_POST["precio2"];
-                  $producto2=$_POST["producto2"];
-
-                  $total=$precio1+$precio2;
-
-
-                  echo("el producto 1 es : ".$producto1."------".$precio1);
-                  echo("<br>");
-                  echo("el producto 2 es : ".$producto2."------".$precio2);
-                  echo("<br>");
-                  echo("<br>");
-                  echo("**************************");
-                  echo("el total de la compra es".$total)
-
-                  
-                  
-          ?>
-          </h5>
-
-       </div>   
-
-        <?php endif ?>
-
+          </div>
  
      </div>
-
-    </div>
-
-
-
-
-
-
 
 
 
 
 </main>
 
-<footer>
+<footer class=" mt-4 ">
+<h3 class="inf">INFORMACION...</h3>
+<div class=" container">
+<div class="row mt-4">
+  <div class="col-12 col-md-6">
+  <p>¿donde nos puede encontrar? : Cra. 50 ##553, Bello, Antioquia</p>
+<p>¿donde llamar? : +57 44542731</p>
+<p>horarios de atencion: todos los dias de 7:00am-20:00pm</p>
+<p>puede seguirnos en: </p>
+<img src="img/in.png" alt="img16" class="img-fluid img2" >
+<img src="img/face.png" alt="img17"class="img-fluid img2" >
+<p>instagram: @Rica_Despensa</p>
+<p>facebook: Despensa_de_bello </p>
+  </div>
+  <div class="col-12 col-md-6">
+  <p>del desarrollador : </p>
+<p>Nombre: karen yuliana valencia ochoa</p>
+<p>¿donde lo encuentran?: </p>
+<img src="img/in.png" alt="img16" class="img-fluid img2" >
+<img src="img/face.png" alt="img17"class="img-fluid img2" >
+<p>instagram: @karen.v.x_x</p>
+<p>facebook: karen valencia </p>
+  </div>
+
+</div>
+</div>
+
 
 </footer>
 
